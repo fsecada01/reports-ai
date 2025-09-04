f7efbd11ff62a73ea0e4b4de6334d711a2d87034
fsecada01 <francis.secada@gmail.com>
2025-09-03 22:06:41 -0400

    Adding build-related files to `.gitignore`

--- build/lib/reports_ai/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import ReportInstance
-
-class ReportInstanceForm(forms.ModelForm):
-    class Meta:
-        model = ReportInstance
-        fields = ['title', 'report_type', 'git_repo_url']
